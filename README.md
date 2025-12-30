2f0e696937a9215224d644676aff336eb4933f26
Ayberk <ayberk.tuncel@icloud.com>
2025-12-31 02:25:43 +0300

    Doviz projesi tamamlandi

---
+++ README.md
@@ -0,0 +1,40 @@
+# 💰 Döviz Kur Hesaplama Programı
+
+Bu proje, Python ve Streamlit kullanılarak geliştirilmiş, anlık piyasa verilerini kullanarak döviz çevirisi yapan bir web uygulamasıdır.
+
+**Frankfurter API** kullanılarak **USD** ve **EUR** kurlarını anlık olarak **TRY** (Türk Lirası) karşılığına çevirir.
+
+## 🚀 Özellikler
+* **Canlı Veri:** Kurlar statik değildir, API üzerinden saniyelik çekilir.
+* **Esnek Giriş:** Kuruşlu hesaplamalar için *float* desteği.
+* **Kolay Arayüz:** Kullanıcı dostu seçim menüleri.
+* **JSON Analizi:** Arka planda gelen karmaşık veri ayıklanarak işlenir.
+
+## 🛠️ Kullanılan Teknolojiler
+* **Python 3.x**
+* **Streamlit** (Arayüz için)
+* **Requests** (API bağlantısı için)
+
+## 💻 Nasıl Çalıştırılır?
+
+1.  Projeyi indirin:
+    ```bash
+    git clone [https://github.com/ayberktuncel/doviz-cevirici.git](https://github.com/ayberktuncel/doviz-cevirici.git)
+    cd doviz-cevirici
+    ```
+
+2.  Gerekli kütüphaneleri yükleyin:
+    ```bash
+    pip install streamlit requests
+    ```
+
+3.  Uygulamayı başlatın:
+    ```bash
+    streamlit run doviz.py
+    ```
+
+## 📸 Örnek Kullanım
+*(Uygulamanın ekran görüntüsünü buraya ekleyebilirsiniz)*
+
+---
+**Geliştirici:** [Ayberk Tuncel](https://github.com/ayberktuncel)(https://www.linkedin.com/in/ayberk-tuncel/)
---
+++ doviz.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 31 01:25:39 2025
+
+@author: Ayberk Tuncel
+"""
+import streamlit as st
+import requests
+
+# Girdilerin Ayarlanması
+st.title("💰 Döviz( USD, EUR to TRY ) Çevirme Programı") #Algoritmada TL yerine TRY kullanılır.
+st.write("Hoş geldin! Çevirilecek para ve birimini çevrilecek olan birimi ayarlayınız.")
+para_miktarı= st.number_input("Çevrilecek olan miktarı giriniz",value=100.0)
+para_birimi=st.selectbox("Para birimini seçiniz",["USD", "EUR"])
+
+#API-JSON kullanımı
+if st.button("Hesapla"):
+    adres= f"https://api.frankfurter.app/latest?from={para_birimi}&to=TRY" #para birimi değişkenini süslü paranteze almayı unutmayın
+    cevap= requests.get(adres)
+    cevap_new = cevap.json() #Karmaşık bilgiyi JSON formatına çevirdik.
+    st.write(cevap_new)
+    #alttaki kodları yazmadan çalıştırırsak kodu cmd aracılığı ile orda json formatında çıktı gözükür. TRY çevirmek için alt başlık olan -rates- seçilir.
+    kur= cevap_new["rates"]["TRY"]
+    sonuc = para_miktarı * kur
+    #sonucu ekrana bastıralım. Tekrar hatırlayalım TRY=TL
+    st.write(para_miktarı, para_birimi, "=", sonuc, "TL")
+    st.write("Anlık Kur: 1", para_birimi, "=", kur, "TL")
+
+# İmza
+st.markdown("---")
+st.markdown("""
+<div style="text-align: center; margin-top: 20px; color: #888;">
+    Tasarlayan: <strong>Ayberk Tuncel</strong>
+</div>
+""", unsafe_allow_html=True)
